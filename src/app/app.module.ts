d6da5ff43e2b97867a404e684439b6b5376e4d91
mahmoud.ragab@cis.asu.edu.eg <mahmo@DESKTOP-FHC66HL>
2019-12-01 15:51:09 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router'
+
+const routes = [
+  {path:'',component:AppComponent},
+]
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
