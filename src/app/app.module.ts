5a119bc4bd07ad162b9354429c9fea6ba7b12527
eslam <eslam.ramadan25@gmail.com>
2019-12-09 16:14:23 +0200

    sloved and not solved exams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router'
+import { RouterModule } from '@angular/router';
+import { SolvedExamsComponent } from './solved-exams/solved-exams.component';
+import { UnSolvedExamsComponent } from './un-solved-exams/un-solved-exams.component';
+import { SavedexamsComponent } from './savedexams/savedexams.component'
+import { ExamservicesService } from './examservices.service';
+import { HttpModule } from '@angular/http';
+ 
 
 const routes = [
-  {path:'',component:AppComponent},
+  {path:'',component:SolvedExamsComponent},
+  {path:'solved',component:SolvedExamsComponent},
+  {path:'notsolved',component:UnSolvedExamsComponent},
+   {path:'SolveExam/:id',component:SolvedExamsComponent}
+  //{path:'savedexams',component:SavedexamsComponent}
+
+
 ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SolvedExamsComponent,
+    UnSolvedExamsComponent,
+    SavedexamsComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-
-    ])
+    HttpClientModule,
+    HttpModule,
+  
+    RouterModule.forRoot(
+      routes
+    )
   ],
-  providers: [],
+  providers: [ExamservicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/exams.ts
@@ -0,0 +1,5 @@
+export class exams {
+    
+    constructor(public ExamID : number, public Instructor :string , public Topic:string,
+       public Course:string,public Degree:string ) {}
+}
