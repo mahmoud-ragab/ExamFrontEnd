84d40510da70f1489000abf2fe50f5a2c8f50f00
mahmoud.ragab@cis.asu.edu.eg <mahmo@DESKTOP-FHC66HL>
2019-12-16 06:35:50 +0200

    ragab edit

--- src/app/solve-exam/solve-exam.component.ts
+++ src/app/solve-exam/solve-exam.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {SolveExamService}  from '../solve-exam.service';
 import {HttpClient} from '@angular/common/http';
 import { LocalStorageService } from '../core/local-storage.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'solve-exam',
   templateUrl: './solve-exam.component.html',
@@ -15,7 +15,8 @@ Postexamurl:string="https://localhost:44354/api/PostExam/";
 user: any;
 
   constructor(private myHttp :HttpClient,
-    private localStorageService: LocalStorageService,private route: ActivatedRoute)
+    private localStorageService: LocalStorageService,private route: ActivatedRoute,
+    private router: Router)
   {
     this.user = JSON.parse(localStorageService.getItem('UserInfo'));
     console.log(this.user)
@@ -37,7 +38,7 @@ user: any;
     console.log(i,f);
   }
   Submit(){
-    
+    debugger;
     let exam_id=this.route.snapshot.paramMap.get('id');
     let Std_id= this.user['Id'];
     let CorrectAnswer=[]
@@ -116,8 +117,8 @@ user: any;
       Answers_id,
       CorrectAnswer
     }
-    this.myHttp.post(this.Postexamurl,{body:ExamDetailes}).subscribe((data)=>{
-      console.log(data)
+    this.myHttp.post(this.Postexamurl,ExamDetailes).subscribe((data)=>{
+      this.router.navigate(['/home'])
     },(error)=>{console.log(error)})
 
   }
